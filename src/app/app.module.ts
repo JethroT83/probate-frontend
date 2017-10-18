62add8f550671c639aec3ffacd42500b00c08ffe
Jed Lynch <jedlynch@gmail.com>
2017-10-18 18:38:35 -0400

    Basic login page.  probate-41

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+	login:{
+		user:string,
+		password:string
+	}
+
+	user:string;
+	password:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(){
+
+
+  }
+
+  onLogin(user,password){
+  	console.log(user, password);
+  }
+}
+
